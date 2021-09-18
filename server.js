a6221828b48e33cb1a1a562053c9760d27f02db0
ravshanov1003 <ravshanov1003@gmail.com>
2021-09-18 23:14:13 +0500

    multer server

--- server.js
+++ server.js
@@ -4,10 +4,31 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
 
+app.post("/uploadfile", upload.single("myFile"), (req, res) => {
+    const file = req.file;
+    const img = fs.readFileSync(file.path);
+    const encoded = img.toString("base64");
+    fs.writeFile("files/out.jpg", encoded, "base64", (err) => {
+        console.log(err);
+    });
+    if (!file) {
+        console.log(Error)
+    } else {
+        res.send();
+    }
+});
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-})
-
+app.post("/uploadmultiple", upload.array("myFiles", 10), (req, res) => {
+    const files = req.files;
+    console.log(files);
+    if (!files) {
+        console.log(Error)
+    } else {
+        res.send({ message: "Files have been saved" });
+    }
+});
 app.listen(3000, _ => console.log('server is running on port 3000'))
